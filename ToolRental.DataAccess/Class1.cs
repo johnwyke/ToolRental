69dfdad1c4fadb9df31b5fbc41d088ffcd98ecc7
johnwyke <johnwyke@mail.weber.edu>
2020-02-13 08:39:08 -0700

    Added Base projects - Changed ApplicationJSON database String to wykeMovie2

---
+++ ToolRental.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ToolRental.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ ToolRental.Utility/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ToolRental.Utility
+{
+    public class Class1
+    {
+    }
+}
