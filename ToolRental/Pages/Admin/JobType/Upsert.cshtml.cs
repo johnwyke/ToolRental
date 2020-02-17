f9a4d033ef4caa467ee64cd46986a49434395a37
johnwyke <johnwyke@mail.weber.edu>
2020-02-17 15:36:22 -0700

    Finished JobType

--- ToolRental/Pages/Admin/JobType/Upsert.cshtml.cs
+++ ToolRental/Pages/Admin/JobType/Upsert.cshtml.cs
@@ -4,14 +4,53 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToolRental.DataAccess.Data.Repository.IRepository;
 
 namespace ToolRental.Pages.Admin.JobType
 {
     public class UpsertModel : PageModel
     {
-        public void OnGet()
-        {
+        public IUnitOfWork _unitOfWork;
 
+        [BindProperty]
+        public Models.JobType JobTypeObj { get; set; }
+        public UpsertModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork; 
+
+        }
+        public IActionResult OnGet(int? id)
+        {
+            JobTypeObj = new Models.JobType();
+            if (id != null)
+            {
+                JobTypeObj = _unitOfWork.JobType.GetFirstORDefault(u => u.Id == id);
+                if(JobTypeObj == null)
+                {
+                    // Cant be found 
+                    return NotFound();
+                }
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if(JobTypeObj.Id == 0)
+            {
+                // Its new
+                _unitOfWork.JobType.Add(JobTypeObj);
+            }
+            else
+            {
+                _unitOfWork.JobType.Update(JobTypeObj);
+            }
+            _unitOfWork.Save();
+            return RedirectToPage("./Index");
         }
     }
 }
--- ToolRental/wwwroot/js/jobtype.js
+++ ToolRental/wwwroot/js/jobtype.js
@@ -1,1 +1,34 @@
-﻿
+﻿var datatabe;
+
+$(document).ready(function(){
+    Load();
+});
+
+function Load() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/jobtype/",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "40%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <a href="/Admin/jobtype/upsert?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px;">
+                                    <i class="far fa-edit"></i> Edit
+                                </a>
+                                <a class="btn btn-danger text-white" style="cursor:pointer; width:100px;" onclick=Delete('/api/jobtype/'+${data})>
+                                    <i class="far fa-trash-alt"></i> Delete
+                                </a>
+                    </div>`;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        }, "width": "100%"
+    });
+}
