5e4849d7bf7ea100b062f54915ccfc238d78de93
johnwyke <johnwyke@mail.weber.edu>
2020-02-26 11:11:42 -0700

    Added Scaffolding -Clean

---
+++ ToolRental/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ToolRental.DataAccess;
+
+[assembly: HostingStartup(typeof(ToolRental.Areas.Identity.IdentityHostingStartup))]
+namespace ToolRental.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
